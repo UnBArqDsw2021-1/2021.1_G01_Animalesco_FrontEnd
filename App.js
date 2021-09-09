2136146eb984dd6112247552e0ffcc3ebdd81ca8
Leonardo Gomes <leonardodasigomes@gmail.com>
2021-09-09 11:26:32 -0300

    Adjust route on App.js

--- App.js
+++ App.js
@@ -1,15 +1,14 @@
 import React from "react";
 import { StatusBar } from "expo-status-bar";
 import { StyleSheet } from "react-native";
-// import Routes from "./src/routes.js";
+import Routes from "./src/routes.js";
 import { NavigationContainer } from "@react-navigation/native";
-import Login from "./src/screens/Login";
 
 export default function App() {
   return (
     <NavigationContainer style={styles.container}>
       <StatusBar style="dark" />
-      <Login />
+      <Routes />
     </NavigationContainer>
   );
 }
