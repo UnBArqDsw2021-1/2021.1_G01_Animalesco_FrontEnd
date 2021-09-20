52d866654a1edb0965edc9e1ffe34e65f7809a1a
daniso123 <dannysoaresdeoliveira@gmail.com>
2021-09-20 20:33:34 -0300

    estilizando a pagina

--- src/screens/PetPerfil/index.js
+++ src/screens/PetPerfil/index.js
@@ -3,12 +3,14 @@ import { setStatusBarStyle } from "expo-status-bar";
 import { View, Text, ActivityIndicator } from "react-native";
 import { useRoute } from "@react-navigation/native";
 import { useService, petService } from "@services";
+import { FontAwesome5 } from "@expo/vector-icons";
 
 import defaultStyles from "@screens/styles.js";
 import colors from "@assets/styles/colors";
 
 import { ModalRegister } from "./components";
 import { Add, GoBackHeader } from "@components";
+import styles from "./styles";
 
 export const PetPerfil = () => {
   const [modalVisible, setModalVisible] = useState(false);
@@ -31,13 +33,17 @@ export const PetPerfil = () => {
     <View style={defaultStyles.page}>
       <GoBackHeader />
       <View style={defaultStyles.container}>
+        <View style={styles.cardImage}>
+          <FontAwesome5 name="paw" size={150} color={colors.primary} />
+        </View>
         {pet ? (
-          <View>
-            <Text>{pet.name} </Text>
-            <Text>{pet.specie_name}</Text>
+          <View style={styles.information}>
+            <Text>Nome : {pet.name} </Text>
+            <Text>Especie: {pet.specie_name}</Text>
             <Text>
-              {pet.sex} {pet.color}
+              Raça: {pet.breed_name}
             </Text>
+            <Text>{pet.color}</Text>
           </View>
         ) : (
           <ActivityIndicator size="large" color={colors.dark} />
--- src/screens/PetPerfil/styles.js
+++ src/screens/PetPerfil/styles.js
@@ -1,7 +1,28 @@
 import { StyleSheet } from "react-native";
+import colors from "@assets/styles/colors";
 
 const styles = StyleSheet.create({
-
-    
+  cardImage: {
+    width: "100%",
+    height: "40%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    borderTopLeftRadius: 5,
+    borderBottomLeftRadius: 5,
+    backgroundColor: colors.secondary,
+    marginTop: -140
+  },
+  information: {
+    width: "95%",
+    height: 100,
+    display: "flex",
+    flexDirection: "column",
+    borderRadius: 5,
+    backgroundColor: colors.light,
+    marginVertical: 5,
+    marginTop: 50,
+    marginLeft: 10,
+  },
 });
 export default styles;
